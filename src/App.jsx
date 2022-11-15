bed8889a7e259b94c64343968dc4c2944b0eabf1
kuba-kowalczykowski-lli <kuba.kowalczykowski@llinformatics.com>
2022-11-15 12:39:41 +0100

    Remove obselote code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import MealForm from './components/meal_form/MealForm';
 import MealDescription from './components/meal_description/MealDescription';
 import { useState } from 'react';
 import useApiPost from './hooks/UseApiPost'
-// import Meal from './middleware/Meal';
 
 function App() {
   const [body, setBody] = useState({query: ""});
